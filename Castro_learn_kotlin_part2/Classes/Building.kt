0f0668b076e62c19ae0d7ffd4fa0c93ea81f5600
Carlo Castro <89119470+caloy31@users.noreply.github.com>
2022-08-28 20:39:42 +0800

    Add files via upload

---
+++ Castro_learn_kotlin_part2/Classes/Building.kt
@@ -0,0 +1,13 @@
+class Building {
+  val yearBuilt = 2016
+  val height = 400 // Feet
+  val type = "Limestone"
+}
+
+fun main() {
+  // Write your code below 🏙
+  var residentialBuilding = Building()
+  println(residentialBuilding.yearBuilt) 
+  println(residentialBuilding.height)
+  println(residentialBuilding.type)
+}
